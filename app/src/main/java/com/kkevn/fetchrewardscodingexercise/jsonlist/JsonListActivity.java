1d9b988bc5531a001f44e717281457a8a648da33
Kevin Kowalski <kowalski.d.kevin@gmail.com>
2021-04-22 21:30:35 -0500

    added RecyclerView reference to activity and layout for JSON list

--- app/src/main/java/com/kkevn/fetchrewardscodingexercise/jsonlist/JsonListActivity.java
+++ app/src/main/java/com/kkevn/fetchrewardscodingexercise/jsonlist/JsonListActivity.java
@@ -1,6 +1,7 @@
 package com.kkevn.fetchrewardscodingexercise.jsonlist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -8,9 +9,13 @@ import com.kkevn.fetchrewardscodingexercise.R;
 
 public class JsonListActivity extends AppCompatActivity {
 
+    private RecyclerView rv_list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_json_list);
+
+        rv_list = findViewById(R.id.rv_list);
     }
 }
