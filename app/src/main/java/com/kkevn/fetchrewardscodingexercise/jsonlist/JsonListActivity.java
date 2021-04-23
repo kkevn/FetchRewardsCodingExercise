d3f998db1505061e63e5f9368f5c23be45a720d2
Kevin Kowalski <kowalski.d.kevin@gmail.com>
2021-04-22 21:24:53 -0500

    added blank activity to be used for displaying JSON list

---
+++ app/src/main/java/com/kkevn/fetchrewardscodingexercise/jsonlist/JsonListActivity.java
@@ -0,0 +1,16 @@
+package com.kkevn.fetchrewardscodingexercise.jsonlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kkevn.fetchrewardscodingexercise.R;
+
+public class JsonListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_json_list);
+    }
+}
