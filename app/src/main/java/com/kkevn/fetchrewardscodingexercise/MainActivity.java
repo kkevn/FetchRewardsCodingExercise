e21d41071b33ee08cdb383b569052345b23ed826
Kevin Kowalski <kowalski.d.kevin@gmail.com>
2021-04-22 22:21:49 -0500

    add basic home page to MainActivity

--- app/src/main/java/com/kkevn/fetchrewardscodingexercise/MainActivity.java
+++ app/src/main/java/com/kkevn/fetchrewardscodingexercise/MainActivity.java
@@ -3,12 +3,31 @@ package com.kkevn.fetchrewardscodingexercise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_instructions_link;
+    private Button b_start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // find and allow instruction link to act as hyperlink
+        tv_instructions_link = findViewById(R.id.tv_instructions_link);
+        tv_instructions_link.setMovementMethod(LinkMovementMethod.getInstance());
+
+        // find and allow start button to load activity that loads and displays the JSON file
+        b_start = findViewById(R.id.b_start);
+        b_start.setOnClickListener(e -> loadJsonAcitivty());
+
+    }
+
+    private void loadJsonAcitivty() {
+
     }
 }
