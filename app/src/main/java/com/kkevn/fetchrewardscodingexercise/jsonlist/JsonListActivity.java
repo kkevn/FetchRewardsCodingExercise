8c752963bbf962fcbafb130dffcac7b21353b6c2
Kevin Kowalski <kowalski.d.kevin@gmail.com>
2021-04-22 23:01:26 -0500

    JSON list activity will now reveal message if no entries load

--- app/src/main/java/com/kkevn/fetchrewardscodingexercise/jsonlist/JsonListActivity.java
+++ app/src/main/java/com/kkevn/fetchrewardscodingexercise/jsonlist/JsonListActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.kkevn.fetchrewardscodingexercise.R;
@@ -12,12 +14,19 @@ public class JsonListActivity extends AppCompatActivity {
 
     private RecyclerView rv_list;
 
+    private boolean entriesFound = false;
+    private TextView tv_no_entries;
+
     private Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_json_list);
+
+        // find and reveal the empty list message if no entries available
+        tv_no_entries = findViewById(R.id.tv_no_entries);
+        tv_no_entries.setVisibility(entriesFound ? View.INVISIBLE : View.VISIBLE);
 
         rv_list = findViewById(R.id.rv_list);
     }
