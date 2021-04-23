f285cea3aaab7d174a6757ae78fd3a892143dcc1
Kevin Kowalski <kowalski.d.kevin@gmail.com>
2021-04-23 04:21:14 -0500

    added hyperlink to code repository

--- app/src/main/java/com/kkevn/fetchrewardscodingexercise/MainActivity.java
+++ app/src/main/java/com/kkevn/fetchrewardscodingexercise/MainActivity.java
@@ -22,7 +22,7 @@ import com.kkevn.fetchrewardscodingexercise.jsonlist.JsonListActivity;
 public class MainActivity extends AppCompatActivity {
 
     // declare variables for elements to interact with
-    private TextView tv_instructions_link;
+    private TextView tv_instructions_link, tv_repository_link;
     private Button b_start;
 
     /**
@@ -38,6 +38,10 @@ public class MainActivity extends AppCompatActivity {
         // find and allow instruction link to act as hyperlink
         tv_instructions_link = findViewById(R.id.tv_instructions_link);
         tv_instructions_link.setMovementMethod(LinkMovementMethod.getInstance());
+
+        // find and allow code repository link to act as hyperlink
+        tv_repository_link = findViewById(R.id.tv_repository_link);
+        tv_repository_link.setMovementMethod(LinkMovementMethod.getInstance());
 
         // find and allow start button to load activity that loads and displays the JSON file
         b_start = findViewById(R.id.b_start);
